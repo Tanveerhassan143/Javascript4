053a2603fa59424b90a04e204ce04edc6e4eb51c
unknown <ranatanveerhassan04@gmail.com>
2023-10-20 18:27:25 +0500

    ok

---
+++ app.js
@@ -0,0 +1,181 @@
+alert("welcome");
+// var greeting;
+// var hour = prompt("enter you time");
+// if (hour <= 12) {
+//     alert("Good day");
+// }
+// else {
+//     alert("Good evening");
+// }
+
+// var enter = prompt("enter you number");
+// if (enter == ["1", "2", "3", "4"]) {
+//     alert("right");
+// }
+// else {
+//     alert("wrong");
+// }
+
+// var std = ["ali","q23421",["sfsc"]]
+// var chak =prompt("enter name like ali,as,ad")
+
+// alert(std[2][0][1][0]);
+
+// else{
+//     alert("pleas correct option");
+// }
+
+
+// enter the food project
+
+
+
+
+
+var food = prompt("enter your favourite food,burger ,pizza,BBQ,Tikabotti");
+if (food == "burger") {
+    var size = prompt("enter your favourite size like S, M ,L  in capital wordl");
+    if (size == "S") {
+        var falv = prompt("enter flaver==>chikan, simple, zinger,");
+        if (falv == "chikan" && "simple" && "zinger") {
+            alert("waiting ! your order is boking, Thank you");
+        }
+        else {
+            alert("please enter correct flaver");
+        }
+
+    }
+    else if (size == "M") {
+        var falv = prompt("enter flaver==>chikan, simple, zinger,");
+        if (falv == "chikan" || "simple" || "zinger") {
+            alert("waiting ! your order is boking, Thank you");
+        }
+        else {
+            alert("please enter correct flaver");
+        }
+    }
+    else if (size == "L") {
+        var falv = prompt("enter flaver==>chikan, simple, zinger,");
+        if (falv == "chikan" || "simple" || "zinger") {
+            alert("waiting ! your order is boking, Thank you");
+        }
+        else {
+            alert("please enter correct flaver");
+        }
+    }
+    else{
+        alert("please enter correct size");
+    }
+
+}
+
+
+else if (food == "pizza") {
+    var size = prompt("enter your favourite size like S, M ,L  in capital wordl");
+    if (size == "S") {
+        var falv = prompt("enter flaver==>chikan, simple, zinger,");
+        if (falv == "chikan" || "simple" || "zinger") {
+            alert("waiting ! your order is boking, Thank you");
+        }
+        else {
+            alert("please enter correct flaver");
+        }
+    }
+    else if (size == "M") {
+        var falv = prompt("enter flaver==>chikan, simple, zinger,");
+        if (falv == "chikan" || "simple" || "zinger") {
+            alert("waiting ! your order is boking, Thank you");
+        }
+        else {
+            alert("please enter correct flaver");
+        }
+    }
+    else if (size == "L") {
+        var falv = prompt("enter flaver==>chikan, simple, zinger,");
+        if (falv == "chikan" || "simple" || "zinger") {
+            alert("waiting ! your order is boking, Thank you");
+        }
+        else {
+            alert("please enter correct flaver");
+        }
+    }else{
+        alert("please enter correct size");
+    }
+}
+
+
+
+else if (food == "BBQ") {
+    var size = prompt("enter your favourite size like S, M ,L  in capital wordl");
+    if (size == "S") {
+        var falv = prompt("enter flaver==>chikan, simple, zinger,");
+        if (falv == "chikan" || "simple" || "zinger") {
+            alert("waiting ! your order is boking, Thank you");
+        }
+        else {
+            alert("please enter correct flaver");
+        }
+    }
+    else if (size == "M") {
+        var falv = prompt("enter flaver==>chikan, simple, zinger,");
+        if (falv == "chikan" || "simple" || "zinger") {
+            alert("waiting ! your order is boking, Thank you");
+        }
+        else {
+            alert("please enter correct flaver");
+        }
+    }
+    else if (size == "L") {
+        var falv = prompt("enter flaver==>chikan, simple, zinger,");
+        if (falv == "chikan" || "simple" || "zinger") {
+            alert("waiting ! your order is boking, Thank you");
+        }
+        else {
+            alert("please enter correct flaver");
+        }
+    }else{
+        alert("please enter correct size");
+    }
+}
+
+
+else if (food == "Tikabotti") {
+    var size = prompt("enter your favourite size like S, M ,L  in capital wordl");
+    if (size == "S") {
+        var falv = prompt("enter flaver==>chikan, simple, zinger,");
+        if (falv == "chikan" || "simple" || "zinger") {
+            alert("waiting ! your order is boking, Thank you");
+        }
+        else {
+            alert("please enter correct flaver");
+        }
+    }
+    else if (size == "M") {
+        var falv = prompt("enter flaver==>chikan, simple, zinger,");
+        if (falv == "chikan" || "simple" || "zinger") {
+            alert("waiting ! your order is boking, Thank you");
+        }
+        else {
+            alert("please enter correct flaver");
+        }
+    }
+    else if (size == "L") {
+        var falv = prompt("enter flaver==>chikan, simple, zinger,");
+        if (falv == "chikan" || "simple" || "zinger") {
+            alert("waiting ! your order is boking, Thank you");
+        }
+        else {
+            alert("please enter correct flaver");
+        }
+    }else{
+        alert("please enter correct size");
+    }
+}
+else {
+    alert("please enter corect food name,like burger,pizza, BBQ, Tikaboti");
+}
+
+
+
+
+
